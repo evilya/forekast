11659d77d219b6b6569d00c088427b5f0d16b139
Ilya Evtushenko <ilya.evtushenko@jetbrains.com>
2024-03-05 14:40:59 +0100

    Confgure ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,17 @@ plugins {
     alias(libs.plugins.buildConfig) apply false
     alias(libs.plugins.ktlint)
 }
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    ktlint {
+        filter {
+            exclude("**/generated/**")
+            exclude("**/*.gradle.kts/**")
+        }
+    verbose.set(true)
+    outputToConsole.set(true)
+    debug.set(true)
+    }
+}
